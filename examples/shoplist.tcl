7be2f4a8febc35d37929c3ac222c86bf9a834b9c
drh <drh@sqlite.org>
2018-02-01 17:28:45 +0000

    Restructure and comment the shoplist.tcl example to make it easier for people to read and understand.
    FossilOrigin-Name: 47e58e7d2b87d5bcbe714308e6c59107cca7a068fd2c370060ecb1b1ad28cf49

--- examples/shoplist.tcl
+++ examples/shoplist.tcl
@@ -18,6 +18,72 @@
 #        way that Wapp supports.
 #
 set DBFILE /shoppinglist.db  ;# Change to name of the database.
+
+# Every page should call this routine first, and abort if this
+# routine returns non-zero.
+#
+# This routine outputs the page header and opens the database file.
+# It checks the login cookie.  If the user is not logged in, this routine
+# paints the login screen and returns 1 (causing the caller page to abort).
+# 
+proc shopping-list-header {} {
+  wapp-trim {
+    <html>
+    <head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="content-type" content="text/html; charset=UTF-8">
+    <link href="%url([wapp-param SCRIPT_NAME]/style.css)" rel="stylesheet">
+    <title>Shopping List</title>
+    </head><body>
+    <h1>Shopping List</h1>
+  }
+  sqlite3 db $::DBFILE
+  db timeout 1000
+  db eval BEGIN
+  if {[wapp-param-exists logout]} {
+    wapp-clear-cookie shopping-list-login
+    set pswd {}
+  } else {
+    set pswd [wapp-param pswd [wapp-param shopping-list-login]]
+  }
+  if {$pswd=="" 
+    || ![db exists {SELECT 1 FROM config WHERE name='password' AND value=$pswd}]
+  } {
+    wapp-trim {
+      <p><form method="POST" action="%url([wapp-param SELF_URL])">
+      Password: <input type="password" name="pswd" width=12>
+      <input type="submit" value="Login"></form></p>
+    }
+    db eval COMMIT
+    db close
+    return 1
+  }
+  if {[wapp-param-exists pswd]} {
+    wapp-set-cookie shopping-list-login $pswd
+  }
+  return 0
+}
+
+# Every page should call this routine at the end to clean up the database
+# connection.
+#
+proc shopping-list-footer {} {
+  db eval COMMIT
+  db close
+}
+
+# The default action is to show the current shopping list.
+#
+# Query parameters may cause changes to the shopping list:
+#    del=ID       Delete shopping list item ID if it exists
+#    add=NAME     Add a new shopping list item called NAME
+#                 but only if there is no existing item with the
+#                 same name
+#    undel=ID     Undo a prior delete of item ID.
+# Only one of the above edit operations may be applied per request.
+# The edit action is applied prior to displaying the shopping list.
+# All edit actions are idempotent.
+#
 proc wapp-default {} {
   if {[shopping-list-header]} return
   set base [wapp-param SCRIPT_NAME]
@@ -65,47 +131,11 @@ proc wapp-default {} {
   }
   shopping-list-footer
 }
-proc shopping-list-header {} {
-  wapp-trim {
-    <html>
-    <head>
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="content-type" content="text/html; charset=UTF-8">
-    <link href="%url([wapp-param SCRIPT_NAME]/style.css)" rel="stylesheet">
-    <title>Shopping List</title>
-    </head><body>
-    <h1>Shopping List</h1>
-  }
-  sqlite3 db $::DBFILE
-  db timeout 1000
-  db eval BEGIN
-  if {[wapp-param-exists logout]} {
-    wapp-clear-cookie shopping-list-login
-    set pswd {}
-  } else {
-    set pswd [wapp-param pswd [wapp-param shopping-list-login]]
-  }
-  if {$pswd=="" 
-    || ![db exists {SELECT 1 FROM config WHERE name='password' AND value=$pswd}]
-  } {
-    wapp-trim {
-      <p><form method="POST" action="%url([wapp-param SELF_URL])">
-      Password: <input type="password" name="pswd" width=12>
-      <input type="submit" value="Login"></form></p>
-    }
-    db eval COMMIT
-    db close
-    return 1
-  }
-  if {[wapp-param-exists pswd]} {
-    wapp-set-cookie shopping-list-login $pswd
-  }
-  return 0
-}
-proc shopping-list-footer {} {
-  db eval COMMIT
-  db close
-}
+
+# This page shows recent purchases with an opportunity to re-add those
+# purchases to the shopping list.  The idea is that to have easy access
+# to common purchases.
+#
 proc wapp-page-common {} {
   if {[shopping-list-header]} return
   set base [wapp-param SCRIPT_NAME]
@@ -120,13 +150,22 @@ proc wapp-page-common {} {
   }
   shopping-list-footer
 }
+
+# The /env page shows the CGI environment.  This is for testing only.
+# There are no links to this page.
+#
 proc wapp-page-env {} {
+  if {[shopping-list-header]} return
   wapp-trim {
     <html>
     <h1>CGI Environment</h1>
     <pre>%html([wapp-debug-env])</pre>
   }
+  shopping-list-footer
 }
+
+# The style-sheet
+#
 proc wapp-page-style.css {} {
   wapp-mimetype text/css
   wapp-cache-control max-age=3600
@@ -141,4 +180,8 @@ proc wapp-page-style.css {} {
     }
   }
 }
+
+# After all pages handling routines have been defined, start up
+# the Wapp handler.
+#
 wapp-start $argv
