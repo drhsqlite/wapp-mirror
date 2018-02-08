bb1ec3fed0828d6a65a3507283a256fba6b1a961
drh <drh@sqlite.org>
2018-02-08 01:24:08 +0000

    Add the "self.tcl" example application that displays a copy of itself.
    FossilOrigin-Name: 8f65cebe601bd2087b796bdda163ec59dd6b46ea398bac52eb58d5d172864042

---
+++ examples/self.tcl
@@ -0,0 +1,70 @@
+# This script demonstrates a Wapp application that can display a copy
+# of itself via the /self page.  (See the wapp-page-self procedure for
+# how that one page is generated.)
+#
+# This script also has a homepage and an /env page that show the wapp
+# environment.  The header and footer for each page is broken out into
+# separate subroutines.
+#
+# Just for grins, there is also a style-sheet.
+#
+package require wapp
+proc common-header {} {
+  wapp-trim {
+    <html>
+    <head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="content-type" content="text/html; charset=UTF-8">
+    <link href="%url([wapp-param SCRIPT_NAME]/style.css)" rel="stylesheet">
+    <title>Wapp Self-Display Demo</title>
+    </head>
+    <body>
+  }
+}
+proc common-footer {} {
+  wapp-trim {
+    </body>
+    </html>
+  }
+}
+proc wapp-default {} {
+  common-header
+  wapp-trim {
+    <h1>Wapp Self-Display Demo</h1>
+    <ul>
+    <li> <a href='%url([wapp-param SCRIPT_NAME])/self'>Show the script
+    that generates this page</a>
+    <li> <a href='%url([wapp-param SCRIPT_NAME])/env'>Wapp Environment</a>
+    </ol>
+  }
+  common-footer
+}
+proc wapp-page-env {} {
+  common-header
+  wapp-trim {
+    <h1>Wapp Environment</h1>
+    <pre>%html([wapp-debug-env])</pre>
+  }
+  common-footer
+}
+proc wapp-page-self {} {
+  common-header
+  set fd [open [wapp-param SCRIPT_FILENAME] rb]
+  set script [read $fd]
+  close $fd
+  wapp-trim {
+    <h1>Wapp Script That Shows A Copy Of Itself</h1>
+    <pre>%html($script)</pre>
+  }
+  common-footer
+}
+proc wapp-page-style.css {} {
+  wapp-mimetype text/css
+  wapp-trim {
+    pre {
+       border: 1px solid black;
+       padding: 1ex;
+    }
+  }
+}
+wapp-start $argv
